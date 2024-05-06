8adfa9c068cf9142ce3368cff4f4b7693d8d8ab6
AleksandraMarasek <232418@student.uek.krakow.pl>
2024-05-06 16:33:21 +0200

    2024-05-06 AM

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/extract')
+def extract():
+    return render_template("extract.html.jinja")
+
+@app.route('/products')
+def products():
+    return render_template("products.html.jinja")
+
+@app.route('/author')
+def author():
+    return render_template("author.html.jinja")
+
+@app.route('/product/<product_id>')
+def product(product_id):
+    return render_template("product.html.jinja", product_id=product_id)
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name="world"):
+    return f"Hello, {name}!"
