e6e9d265c422b194b0557d8f411a6c7d23889253
AleksandraMarasek <232418@student.uek.krakow.pl>
2024-05-13 16:37:52 +0200

    2024-05-13 AM

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import requests
+from bs4 import BeautifulSoup
 
 
 @app.route('/')
@@ -7,21 +9,37 @@ from flask import render_template
 def index():
     return render_template("index.html")
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST','GET'])
 def extract():
-    return render_template("extract.html.jinja")
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        url = f"https://www.ceneo.pl/{product_id}/opinie-1"
+        response = requests.form.get(url=url)
+        if response.status_code == requests.codes['ok']:
+            page_dom = BeautifulSoup(response.text, "html.parsel")
+            try:
+                opinions_count = page_dom.select_one('a.product-review_link > span').get_text().strip()
+            except AttributeError:
+                opinions_count = 0
+            if opinions_count:
+                return redirect(url_for('product',product_id=product_id))
+            error= "Brak opinii dla danego produktu"
+            return render_template('extract.html', error=error)
+        error= "Taki produkt nie istnieje"
+        return render_template('extract.html', error=error)
+    return render_template("extract.html")
 
 @app.route('/products')
 def products():
-    return render_template("products.html.jinja")
+    return render_template("products.html")
 
 @app.route('/author')
 def author():
-    return render_template("author.html.jinja")
+    return render_template("author.html")
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    return render_template("product.html.jinja", product_id=product_id)
+    return render_template("product.html", product_id=product_id)
 
 @app.route('/hello')
 @app.route('/hello/<name>')
